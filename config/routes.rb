3b88b93411a55dfa83de46a415a7a825c0013968
Kate Fulton <katesuzannefulton@gmail.com>
2014-12-23 16:31:56 -0800

    Create user sign up functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+
+  get 'users/sign_in', to: 'users#create', as: :new_user
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
