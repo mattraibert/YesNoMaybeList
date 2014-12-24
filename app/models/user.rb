aa6a8396cdc2319c5be2350cb3430c6213612627
Kate Fulton <katesuzannefulton@gmail.com>
2014-12-23 16:22:32 -0800

    Add email validation, attr_access, and has_secure_password to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ActiveRecord::Base
+  has_secure_password
+
+  attr_accessible :email, :password, :Password_confirmation
+
+  validates_uniqueness_of :email
+  #validate email format
+
 end
