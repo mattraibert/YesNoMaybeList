5a146ea393ed25a2efd6223bf6025cef442578ae
Kate Fulton <katesuzannefulton@gmail.com>
2014-12-24 12:09:23 -0800

    Add gray-back class for password reset js functionality

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,16 +20,18 @@
 $(function() {
   $(".reset-password").click(function(event) {
     event.preventDefault();
-    //hard coded stuff in form??
     var resetPasswordForm = '<div><form accept-charset="UTF-8" action="/reset_password" method="post"><div style="display:none"><input name="utf8" type="hidden" value="✓"><input name="authenticity_token" type="hidden" value="hnR+Y3/CAJa9trXUdZWLfQw6LkKHPO39/k6+g/Z0gjc="></div><input id="email" name="email" type="text" oldautocomplete="remove" autocomplete="off"><input name="commit" type="submit" value="Reset Password"></form><a href="#" class="remove-field">Close</a></div>';
-    $(".reset-password").hide();//change .reset-password class to make inactive
+    $(".reset-password").hide();
     $(".password-form").append(resetPasswordForm);
+    $(".log-in").children('form').addClass("gray-back");
+    //Gray back rest of login form?
 
     $(".password-form").on("click", ".remove-field", function(event) {
-    event.preventDefault();
+      event.preventDefault();
 
-    $(this).parent('div').remove();
-    $(".reset-password").show();
+      $(this).parent('div').remove();
+      $(".reset-password").show();
+      $(".log-in").children('form').removeClass("gray-back");
     });
   });
 });
